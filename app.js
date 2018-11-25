b3731be0f7c987a4ac550c8fba26ec7690282eea
Meredith Fontana <meredith@Merediths-MBP.Home>
2018-11-25 14:51:46 -0700

    made two routes: /yoga and /yoga/:id to access json data of yoga poses

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const app = express()
 const port = process.env.PORT || 4000
 const yogaData = require('./data/yogaData')
 
-app.get('/', (req, res) => {
-    res.send('hello world!!!')
+app.get('/yoga', (req, res) => {
+    res.send(yogaData)
 })
+
+
+app.get('/yoga/:id', (req, res, next) => {
+    let id = req.params.id
+    var yogaPose = yogaData.filter(pose => {
+        return pose.id == id
+    })
+        res.send({yogaData: yogaPose[0]})
+    
+}) 
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
