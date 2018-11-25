b444adbc4d902700845b51ed73680a8a73f7cd30
Meredith Fontana <meredith@Merediths-MBP.Home>
2018-11-25 14:45:16 -0700

    added yogaData.json file to application

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const yogaData = require('./data/yogaData')
 
 app.get('/', (req, res) => {
     res.send('hello world!!!')
